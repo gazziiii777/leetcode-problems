def290d2a32fc816c50aa3355b66076d47e7f11a
michelangeloiiii777 <michelangeloiiii777@gmail.com>
2023-12-20 04:15:54 +0300

    Переименовал переменные в соответствии с pep8

--- longestCommonPrefix/longestCommonPrefix.py
+++ longestCommonPrefix/longestCommonPrefix.py
@@ -1,12 +1,16 @@
 # https://leetcode.com/problems/longest-common-prefix/
 # https://leetcode.com/submissions/detail/1121419703/
-strs = ["dog", "racecar", "car"]
 
-answer = ""
-sortedStrs = sorted(strs)
-for i in range(min(len(sortedStrs[0]), len(sortedStrs[-1]))):
-    if sortedStrs[0][i] == sortedStrs[-1][i]:
-        answer += sortedStrs[0][i]
-        continue
-    break
-print(answer)
+
+def longest_common_prefix(strs):
+    answer = ""
+    sorted_strs = sorted(strs)
+    for i in range(min(len(sorted_strs[0]), len(sorted_strs[-1]))):
+        if sorted_strs[0][i] == sorted_strs[-1][i]:
+            answer += sorted_strs[0][i]
+            continue
+        break
+    return answer
+
+
+print(["dog", "racecar", "car"])
--- maxProductDifference/maxProductDifferenceSimply.py
+++ maxProductDifference/maxProductDifferenceSimply.py
@@ -1,7 +1,8 @@
-nums = [4,2,5,9,7,4,8]
+# https://leetcode.com/problems/maximum-product-difference-between-two-pairs/submissions/?envType=daily-question&envId=2023-12-18
 
-def maxProductDifference(nums):
-    numsSorted = sorted(nums)
-    return numsSorted[-1] * numsSorted[-2] - numsSorted[0] * numsSorted[1]
+def max_product_difference(nums):
+    nums_sorted = sorted(nums)
+    return nums_sorted[-1] * nums_sorted[-2] - nums_sorted[0] * nums_sorted[1]
 
-print(maxProductDifference(nums))
+
+print(max_product_difference([4, 2, 5, 9, 7, 4, 8]))
