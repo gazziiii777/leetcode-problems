36c15ad21776821f38368bca673346ef9a68c2d3
michelangeloiiii777 <michelangeloiiii777@gmail.com>
2023-12-21 00:27:48 +0300

    Добавил решение задачи searchInsert, задача решена с помощью бинарного поиска

---
+++ lvlEasy/searchInsert/searchInsert.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/search-insert-position
+def search_insert(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+
+
+print(search_insert([1, 3, 5, 6], 2))
